c6c65b163cf5e67f6f75cce1f03ef28184611b26
Thomas Carl Alexander Dänhardt <thomas.danhardt@gmail.com>
2022-02-22 15:08:21 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orm_CodeFirst_Cocktails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
